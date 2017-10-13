adf9c09714c78c7abfb0fcbd6cd05d9f1275642c
buurz <buurzx@gmail.com>
2017-10-13 16:07:45 +0300

    Parse service refactor

--- app/services/parser_service.rb
+++ app/services/parser_service.rb
@@ -1,21 +1,46 @@
 # frozen_string_literal: true
 
-# rubocop: disable Metrics/AbcSize
-
 require 'rubyXL'
 
 class ParserService
-  attr_accessor :file, :error, :user, :prod_ids
+  attr_accessor :file, :error, :user
 
   def initialize(file:, user: nil)
     @file = file&.tempfile
     @user = user
-    @prod_ids = []
   end
 
   def parse_and_create
-    error!('Файл не может быть пустым') && return if file.blank?
+    error!(I18n.t('parse_service.file.errors.empty')) && return if file.blank?
 
+    prod_ids = fetch_products.map! do |product|
+      updated_product = update_or_create_product(product)
+      break unless updated_product
+      # add to organization
+      user.organization.products << updated_product
+      # add to array for further process
+      updated_product.id
+    end
+
+    destroy_old_products(prod_ids)
+  end
+
+  private
+
+  def destroy_old_products(prod_ids)
+    organization.products.where.not(id: prod_ids).destroy_all
+  end
+
+  def update_or_create_product(product)
+    prod = organization.products.find_or_create_by(name: product[:name])
+    error!(prod.errors.full_messages) && return unless prod.valid?
+
+    prod.tap do |p|
+      p.update_column(:quantity, product[:quantity])
+    end
+  end
+
+  def fetch_products
     workbook = RubyXL::Parser.parse(file)
     worksheet = workbook[0]
 
@@ -23,20 +48,12 @@ class ParserService
 
     worksheet.each do |row|
       break unless row.cells.first.value
-      products << { name: row.cells.first.value,
+
+      products << { name:     row.cells.first.value,
                     quantity: row.cells.second.value }
     end
 
-    products.each do |p|
-      prod = organization.products.find_or_create_by(name: p[:name])
-      error!(prod.errors.full_messages) && break unless prod.valid?
-
-      prod.update(quantity: p[:quantity])
-      user.organization.products << prod
-      prod_ids << prod.id
-    end
-
-    organization.products.where.not(id: prod_ids).destroy_all
+    products
   end
 
   def organization
